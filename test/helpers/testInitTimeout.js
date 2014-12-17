3b58604327968e465cabd04bf617162b760d9b52
Kevin Smithson <ksmithson@pocketly.com>
2014-12-17 14:26:39 -0800

    moved files

--- test/helpers/testInitTimeout.js
+++
@@ -1,8 +0,0 @@
-var sand = require('../..');
-var BadGrain = require('./BadGrain');
-
-sand({
-  initTimeout: 100
-}).use(BadGrain, {
-  testInit: true
-}).start();
--- test/helpers/testShutdownTimeout.js
+++
@@ -1,8 +0,0 @@
-var sand = require('../..');
-var BadGrain = require('./BadGrain');
-
-sand({
-  shutdownTimeout: 100
-}).use(BadGrain, {
-  testShutdown: true
-}).start().shutdown();
