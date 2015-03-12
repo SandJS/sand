1efa5c76149bd64cd8803a5110e7a4cba318ddcf
Kevin Smithson <ksmithson@sazze.com>
2015-03-12 08:43:36 -0700

    removed class/extend

--- test/extend/class/Emitter.js
+++
@@ -1,9 +0,0 @@
-var Class = require('../../..').Class;
-var EventEmitter = require('events').EventEmitter;
-
-exports = module.exports =
-Class.extend(EventEmitter, {
-  sendEvent: function(name) {
-    this.emit(name);
-  }
-});
